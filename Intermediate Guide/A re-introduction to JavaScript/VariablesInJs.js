c194b87657a264522a7e39e90854be9d35c867a4
Cheruiyot Haron <96364614+harontoo@users.noreply.github.com>
2022-05-26 11:20:42 +0300

    Variables In JavaScript

---
+++ Intermediate Guide/A re-introduction to JavaScript/VariablesInJs.js
@@ -0,0 +1,10 @@
+//Variables in JavaScript
+let a,c;
+{
+    let a =100;
+    var b =100;
+    const c =1000;    
+    console.log(`a :${a}\nb :${b}\nc :${c}`);
+}
+
+console.log(`a :${a}\nb :${b}\nc :${c}`);
