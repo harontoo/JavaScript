2e4cab5b518a18a5c4b74195bd29f27391e58616
Cheruiyot Haron <96364614+harontoo@users.noreply.github.com>
2022-05-26 11:04:53 +0300

    Other DataTypes in JavaScript.

---
+++ Intermediate Guide/A re-introduction to JavaScript/OtherTypes.js
@@ -0,0 +1,21 @@
+//Other types in JavaScript
+let a;
+let b = null;
+
+console.log(`a :${a}\nb :${b}`);
+
+//Boolean DataTypes in Java script;
+let  vala = true;
+let  valb = false;
+
+console.log(`vala :${vala}\nvalb :${valb}`);
+
+//true and false values a.k.a "truthy and falsy"
+console.log("1"&&10);
+console.log("1"||10);
+console.log(!"");
+
+
+
+
+
