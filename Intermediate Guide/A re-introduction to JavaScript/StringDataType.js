d43ca50d3c9720f2fa0d73f486b7bada5bbe9ef3
Cheruiyot Haron <96364614+harontoo@users.noreply.github.com>
2022-05-26 10:45:06 +0300

    The String DataType in JavaScript

---
+++ Intermediate Guide/A re-introduction to JavaScript/StringDataType.js
@@ -0,0 +1,7 @@
+//String DataType in JavaScript
+console.log('hello'.length);
+
+//Strings are Objects
+console.log('hello'.charAt(0)); // "h"
+console.log('hello, world'.replace('world', 'mars')); // "hello, mars"
+console.log('hello'.toUpperCase()); // "HELLO"
