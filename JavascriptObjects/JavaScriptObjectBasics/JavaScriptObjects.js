b66f14cbc32ee36d7e3cdf2c8743c5e9139525ad
Cheruiyot Haron <96364614+harontoo@users.noreply.github.com>
2022-06-10 12:05:32 +0300

    Introduced JS Objects

---
+++ JavascriptObjects/JavaScriptObjectBasics/JavaScriptObjects.js
@@ -0,0 +1,15 @@
+const Person = {
+    name:['Bob','Smith'],
+    age:32,
+    bio:function () {
+        console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+    },
+    introduceSelf: function(){
+        console.log(`Hi,I'm ${this.name[1]}.`);
+    }
+
+};
+
+console.log(Person);
+Person.bio();
+Person.introduceSelf();
