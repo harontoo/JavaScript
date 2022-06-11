bf3a4d334f865f06eafd86675d83bfbcffdf4915
Cheruiyot Haron <96364614+harontoo@users.noreply.github.com>
2022-06-11 13:39:32 +0300

    classes in JavaScript

--- JavascriptObjects/JavaScriptObjectBasics/JavaScriptObjects.js
+++ JavascriptObjects/JavaScriptObjectBasics/JavaScriptObjects.js
@@ -11,5 +11,33 @@ const Person = {
 };
 
 console.log(Person);
+//dot notation
 Person.bio();
 Person.introduceSelf();
+//bracket notation
+console.log(Person['age']);
+
+Person['height'] = 24;
+console.log(Person.height);
+console.log({[Person.name[0]]:'Shmurda'});
+
+//Constructor
+function PersonCtr(name) {
+    this.name = name;
+    this.introduceSelf = function() {
+      console.log(`Hi! I'm ${this.name}.`);
+    }
+  }
+
+  const salva = new PersonCtr('Salva');
+  salva.name;
+  salva.introduceSelf();
+  
+  const frankie = new PersonCtr('Frankie');
+  frankie.name;
+  frankie.introduceSelf();
+  const s = salva;
+  s.name="sw"
+  salva.introduceSelf();
+
+  console.log(salva==s);
